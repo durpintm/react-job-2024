ef88c8f82eb7bf265cd34efe8f13e76028e2b25a
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-23 00:06:41 -0400

    Add edit functionality

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -2,6 +2,7 @@
 import { useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const JobPage = ({ deleteJob }) => {
   const job = useLoaderData();
@@ -29,6 +30,7 @@ const JobPage = ({ deleteJob }) => {
     const confirm = window.confirm("Are you sure!");
     if (!confirm) return;
     deleteJob(jobId);
+    toast.success("Job Deleted Successfully!");
     navigate("/jobs");
   };
   return (
@@ -98,7 +100,7 @@ const JobPage = ({ deleteJob }) => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/jobs/edit/${job.id}`}
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
